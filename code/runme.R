1bb9a6216917160b27714de8dc8882c527ef5ac8
Rawan Albusayli <rawan@live.ru>
2023-01-26 15:24:22 +0300

    Update current files!

--- code/runme.R
+++ code/runme.R
@@ -23,8 +23,6 @@ x<-0
 calculate_survival_data_with_two_cohort(train_file, test_file, x, methods,group_categories)
 # calculate_survival_data_train_valid_test_with_CV(x=x,runs=10,methods,group_categories=group_categories,file_init_train="train_",file_init_valid="valid_",file_init_test="test_")
 # calculate_survival_data_train_valid_with_CV(x=x,runs=10,methods,group_categories=group_categories,file_init_train="train_",file_init_valid="valid_")
-# calculate_survival_data_with_feature_combinations_CV(set, methods,group_categories,folds_threshold=7,csv_file_name='..',combs_number=3,runs=10,file_init_train="train_",file_init_valid="valid_")
-# calculate_survival_data_with_two_cohort_cases_removal(train_file, test_file, x, methods,group_categories)
 
 
 #--------------------- The code to use the function in plots.R file ----------------------#
